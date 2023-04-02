359549a11db34250c2f0fc5866a940a8c1953b46
Георги Сталев <sttallev99@gmail.com>
2023-04-02 17:36:57 +0300

    create errors handle middlaware

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const errorHandler = require('./middleware/error')
 
 //database connecton
 mongoose.connect(process.env.DATABASE, {
@@ -14,6 +17,20 @@ mongoose.connect(process.env.DATABASE, {
     useFindAndModify: false
 }).then(() => console.log('DB connected!'))
     .catch((err) => console.log(err));
+
+
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({
+    limit: '5mb',
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+//error middleware
+app.use(errorHandler);
 
 //port
 const port  = process.env.PORT || 9000;
