eda0e5a0c598c05d260bf331e79fdc17072250a7
Георги Сталев <sttallev99@gmail.com>
2023-04-04 23:20:56 +0300

    create home game with header and footer components and render Mui navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
-function App() {
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import { theme } from './theme';
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home /> }/>
+            <Route path='*' element={<NotFound /> }/>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>    
+    </>
+  )
 }
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Home page</h1>
+    </>
+  )
+}
+
+export default Home
