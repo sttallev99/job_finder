def9685c9915de0a736dc3688fa8440c16734f34
Георги Сталев <sttallev99@gmail.com>
2023-04-05 23:07:48 +0300

    implement logic for create job type into controller

--- server/controllers/jobTypeController.js
+++ server/controllers/jobTypeController.js
@@ -1,1 +1,19 @@
-const jobType = require('../models/jobTitleModel');
+const JobType = require('../models/jobTitleModel');
+const ErrorResponse = require('../utils/errorResonse');
+
+//create job category
+exports.createJobType = async(req, res, next) => {
+    try {
+        const jobT = await JobType.create({
+            jobTitleName: req.body.jobTypeName,
+            user: req.user.id
+        });
+
+        res.status(201).json({
+            success: true,
+            jobT
+        })
+    } catch (error) {
+        next(error);
+    }
+}
