f0c804f6f0cfbfbd7a92b5a60515359d9d1b678d
Георги Сталев <sttallev99@gmail.com>
2023-04-06 22:21:16 +0300

    add isAdmin route protection for create and update job

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { isAuthenticated } = require('../middleware/auth');
+const { isAuthenticated, isAdmin } = require('../middleware/auth');
 const { createJob, singleJob, updateJob } = require('../controllers/jobsController');
 
 
 
 //user routes
 // /api/job/create
-router.post('/jobs/create', isAuthenticated, createJob);
+router.post('/jobs/create', isAuthenticated, isAdmin, createJob);
 // /api/job/id
 router.get('/jobs/:id', singleJob);
 // /api/job/update/job_id
-router.put('/jobs/update/:job_id', isAuthenticated, updateJob);
+router.put('/jobs/update/:job_id', isAuthenticated, isAdmin, updateJob);
 
 
 module.exports = router;
