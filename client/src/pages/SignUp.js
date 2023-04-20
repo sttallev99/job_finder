f816fb039535b6239a4f2076a2ff81bf1c235af0
Георги Сталев <sttallev99@gmail.com>
2023-04-20 16:06:12 +0300

    validate fields into a registration user form

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,34 +1,48 @@
 import React from 'react'
 import * as yup from 'yup';
+import { useFormik } from 'formik';
 import { Avatar, Box, Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from '@mui/material';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-// const validationSchema = yup.object({
-//     firstName: yup
-//         .string('Enter your first name')
-//         .required('First name is required'),
-//     lastName: yup
-//         .string('Enter your Last name')
-//         .required('Last name is required'),
-//     email: yup
-//         .string('Enter your email')
-//         .email('Enter a valid email')
-//         .required('Email is required'),
-//     password: yup
-//         .string('Enter your password')
-//         .password('Enter a valid password')
-//         .required('Password is required'),
-// });
+const validationSchema = yup.object({
+    firstName: yup
+        .string('Enter your first name')
+        .required('First name is required'),
+    lastName: yup
+        .string('Enter your Last name')
+        .required('Last name is required'),
+    email: yup
+        .string('Enter your email')
+        .email('Enter a valid email')
+        .required('Email is required'),
+        password: yup
+        .string('Enter your password')
+        .min(6, 'Password should be of minimum 6 characters length')
+        .required('Password is required')
+    });
 
 const SignUp = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: ''
+        },
+        validationSchema: validationSchema,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null ,2));
+        }
+    })
   return (
     <>
         <Navbar />
         <Box sx={{ height: '81vh', display: "flex", alignItems: "center", justifyContent: "center", bgcolor: "primary.white" }}>
-            <Box component="form" className='form_style border-style' >
+            <Box component="form" onSubmit={formik.handleSubmit} className='form_style border-style' >
                 <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%'}}>
                     <Avatar sx={{ m: 1, bgcolor: 'primary.main', mb: 3}}>
                         <PersonAddIcon />
@@ -49,6 +63,12 @@ const SignUp = () => {
                             shrink: true,
                         }}
                         placeholder="First name"
+                        value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+                        helperText={formik.touched.firstName && formik.errors.firstName}
+
                     />
                     <TextField
                         sx={{
@@ -61,11 +81,16 @@ const SignUp = () => {
                         fullWidth
                         id='lastName'
                         label='Last Name'
-                        name='firstName'
+                        name='lastName'
                         InputLabelProps={{
                             shrink: true,
                         }}
                         placeholder="Last name"
+                        value={formik.values.lastName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+                        helperText={formik.touched.lastName && formik.errors.lastName}
                     />
                     <TextField
                         sx={{
@@ -83,6 +108,11 @@ const SignUp = () => {
                             shrink: true,
                         }}
                         placeholder="E-mail"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
                     />
                     <TextField
                         sx={{
@@ -101,15 +131,20 @@ const SignUp = () => {
                             shrink: true,
                         }}
                         placeholder="Password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+
                     />
-                    <FormControl>
-                        <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+                    <FormControl sx={{ display: 'flex', alignItems: "center", justifyContent: "center", }}>
+                        <FormLabel id="demo-row-radio-buttons-group-label">Create admin account?</FormLabel>
                         <RadioGroup
                             row
                             aria-labelledby="demo-row-radio-buttons-group-label"
                             name="row-radio-buttons-group"
                         >
-                            <FormControlLabel value='0' control={<Radio />} label="Regular user" checked/>
                             <FormControlLabel value='1' control={<Radio />} label="Admin" />
                         </RadioGroup>
                     </FormControl>
